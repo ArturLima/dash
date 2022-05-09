c91ff7918e96b7d46695de32d9183e8f6a01711f
Artur <arturpeixotoo@gmail.com>
2022-05-09 19:54:13 -0300

    add mutation

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -11,10 +11,14 @@ import {
 import Link from "next/link";
 import { SubmitHandler, useForm } from "react-hook-form";
 import * as yup from "yup";
+import { useMutation } from "react-query";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Siderbar";
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
+import { useRouter } from "next/router";
 
 type CreateUserFormData = {
   name: string;
@@ -36,6 +40,24 @@ const createUserFormData = yup.object().shape({
 });
 
 export default function UserCreate() {
+  const router = useRouter();
+
+  const createUser = useMutation(
+    async (user: CreateUserFormData) => {
+      const response = await api.post("users", {
+        user: {
+          ...user,
+          created_at: new Date(),
+        },
+      });
+      return response.data.user;
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("users");
+      },
+    }
+  );
   const { register, handleSubmit, formState } = useForm({
     resolver: yupResolver(createUserFormData),
   });
@@ -45,8 +67,8 @@ export default function UserCreate() {
   const handleCreateUser: SubmitHandler<CreateUserFormData> = async (
     values
   ) => {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-    console.log(values);
+    await createUser.mutateAsync(values);
+    router.push("/users");
   };
   return (
     <Box>
